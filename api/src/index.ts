46b804ae7ca28b8470e8353f8ec100a028bdfc3a
nmfairus-jtm <nmfairus@jtm.gov.my>
2026-06-16 14:21:27 +0800

    add better auth

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,10 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { auth } from './lib/auth.js';
 
 const app = new Hono()
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
