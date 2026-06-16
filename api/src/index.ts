ee38736f0b21351dea71348875127fb6abf26b1e
nmfairus-jtm <nmfairus@jtm.gov.my>
2026-06-16 14:28:09 +0800

    add admin plugin

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,56 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { auth } from './lib/auth.js';
+import { cors } from 'hono/cors';
 
-const app = new Hono()
+const app = new Hono<{
+	Variables: {
+		user: typeof auth.$Infer.Session.user | null;
+		session: typeof auth.$Infer.Session.session | null
+	}
+}>();
+
+app.use(
+	"/api/auth/*",
+	cors({
+		origin: "http://localhost:5173",
+		allowHeaders: ["Content-Type", "Authorization"],
+		allowMethods: ["POST", "GET", "OPTIONS"],
+		exposeHeaders: ["Content-Length"],
+		maxAge: 600,
+		credentials: true,
+	}),
+);
+
+app.use("*", async (c, next) => {
+	const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  	if (!session) {
+    	c.set("user", null);
+    	c.set("session", null);
+    	await next();
+        return;
+  	}
+  	c.set("user", session.user);
+  	c.set("session", session.session);
+  	await next();
+});
 
 app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
-})
+});
+
+app.get("/session", (c) => {
+	const session = c.get("session")
+	const user = c.get("user")
+	
+	if(!user) return c.body(null, 401);
+  	return c.json({
+	  session,
+	  user
+	});
+});
 
 serve({
   fetch: app.fetch,
--- api/src/lib/auth.ts
+++ api/src/lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db  from "../db/index.js";
 import * as schema from "../db/schema.js";
+import { admin } from "better-auth/plugins/admin";
+import { organization } from "better-auth/plugins/organization";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
@@ -10,5 +12,13 @@ export const auth = betterAuth({
     }),
     emailAndPassword: { 
     enabled: true, 
-  }, 
+  },
+  advanced: {
+    crossSubDomainCookies: {
+      enabled: true
+    }
+  },
+  plugins: [
+        admin(), organization()
+    ]
 });
